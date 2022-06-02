10de356f9402705eac5057ae41107c5183b00bbb
motitory <motitory@gmail.com>
2022-06-02 14:42:35 +0900

    fixed error

--- Node/learn-express/app.js
+++ Node/learn-express/app.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
 const nunjucks = require('nunjucks');
-var fs = require('fs')
+
 dotenv.config();
 const indexRouter = require('./routes');
 const userRouter = require('./routes/user');
@@ -26,14 +26,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 console.log(__dirname)
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
 nunjucks.configure('views', {
   express: app,
   watch: true,
 });
-
-
-
 sequelize.sync({ force: false })
   .then(() => {
     console.log('데이터베이스 연결 성공');
@@ -86,12 +83,12 @@ app.use((req, res, next) => {
   next(error);
 });
 
-// app.use((err, req, res, next) => {
-//   res.locals.message = err.message;
-//   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 
 
