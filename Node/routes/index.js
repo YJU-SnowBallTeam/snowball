fe2b2d4f571fe8a07bc0210116af5f45e579395d
ingyoe <melon7256@naver.com>
2022-06-11 22:12:54 +0900

    index

--- Node/routes/index.js
+++ Node/routes/index.js
@@ -74,6 +74,100 @@ router.get("/Notice", async (req, res, next) => {
   res.render("Notice/notice", { Notices, user });
 });
 
+// 공지사항 전체 리스트
+router.get("/Notice", async (req, res, next) => {
+  const user = await req.session.IsLogined;
+  const notices = await Notice.findAll({
+    order: [["post_id", "DESC"]],
+    offset: 0,
+  });
+  notices.forEach((el) => {
+    el.dataValues.date = moment(el.dataValues.date).format("YYYY-MM-DD");
+  });
+  console.log(notices);
+  res.render("Notice/notice", { notices, user });
+});
+
+// 공지사항 작성창
+router.get("/notice-add", (req, res) => {
+  const user = req.session.IsLogined;
+  res.render("Notice/notice-add", { user });
+});
+
+// 공지사항 작성
+router.post("/Notice", async (req, res, next) => {
+  try {
+    let body = req.body;
+    await Notice.create({
+      title: body.inputTitle,
+      content: body.inputContent,
+      noticer: body.inputWriter,
+    });
+    res.redirect("/Notice");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// 공지사항 상세페이지
+router.get("/Notice/:postId", async (req, res, next) => {
+  try {
+    const user = await req.session.IsLogined;
+    const { postId } = req.params;
+    const notice = await Notice.findOne({ where: { post_id: postId } });
+    res.render("Notice/notice-detail", { notice, user });
+  } catch (err) {
+    // error 처리
+    next(err);
+  }
+});
+
+// 공지사항 업데이트 작성창
+router.get("/Notice/update/:postId", async (req, res, next) => {
+  const user = await req.session.IsLogined;
+  const { postId } = req.params;
+  const notice = await Notice.findOne({ where: { post_id: postId } });
+  res.render("Notice/notice-update", { notice, user });
+});
+
+// 공지사항 업데이트
+router.put("/Notice/update/:postId", async (req, res, next) => {
+  try {
+    let body = req.body;
+    console.log("=========================");
+    console.log(body);
+    console.log(body.inputContent);
+    console.log("=========================");
+    await Notice.update(
+      {
+        //   post_id: this.post_id,
+        title: body.inputTitle,
+        content: body.inputContent,
+        boarder: body.inputWriter,
+        //   date: now,
+      },
+      {
+        where: { post_id: req.params.postId },
+      }
+    );
+    res.redirect("/Notice/" + req.params.postId);
+    return res.status(200).send("success");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// 공지사항 삭제
+router.delete("/Notice/delete/:postId", async (req, res, next) => {
+  const { postId } = req.params;
+  console.log("====================");
+  console.log(postId);
+  console.log("====================");
+  const notice = await Notice.destroy({ where: { post_id: postId } });
+  return res.send("success");
+});
+
+// =========================================================================================================================
 // 커뮤니티 전체 리스트
 router.get("/Community", async (req, res, next) => {
   const user = await req.session.IsLogined;
@@ -87,16 +181,17 @@ router.get("/Community", async (req, res, next) => {
   res.render("Community/Community", { boarders, user });
 });
 
+// 커뮤니티 작성 창
 router.get("/Community-add", (req, res) => {
   const user = req.session.IsLogined;
   res.render("Community/Community-add", { user });
 });
 
-// 글 작성
+// 커뮤니티 글 작성
 router.post("/Community", async (req, res, next) => {
   try {
     let body = req.body;
-     console.log(body);
+    //  console.log(body);
     //  console.log(body.inputContent);
     await Board.create({
       title: body.inputTitle,
@@ -104,7 +199,7 @@ router.post("/Community", async (req, res, next) => {
       boarder: body.inputWriter,
     });
     res.redirect("/Community");
-    
+    //  return res.status(200).send("success");
   } catch (err) {
     next(err);
   }
@@ -261,9 +356,113 @@ router.delete(
   }
 );
 
+// 삭제
+router.delete("/Community/delete/:postId", async (req, res, next) => {
+  const { postId } = req.params;
+  console.log("====================");
+  console.log(postId);
+  console.log("====================");
+  const board = await Board.destroy({ where: { post_id: postId } });
+  return res.send("success");
+});
+
+// 삭제 comment
+router.delete(
+  "/Community/comment/delete/:commentId",
+  async (req, res, next) => {
+    const { commentId } = req.params;
+    const comment = await Comment.destroy({ where: { comment_id: commentId } });
+    // res.redirect("/Community/" + req.params.postId);
+    return res.send("delete comment success");
+  }
+);
+
 router.get('/History', (req, res) =>{
   res.render('History/HistoryPage')
 })
+
+
+// FAQ
+// FAQ 전체 리스트
+router.get("/Faq", async (req, res, next) => {
+  const user = await req.session.IsLogined;
+  const faqs = await Faq.findAll({
+    order: [["post_id", "DESC"]],
+    offset: 0,
+  });
+  faqs.forEach((el) => {
+    el.dataValues.date = moment(el.dataValues.date).format("YYYY-MM-DD");
+  });
+  console.log(faqs);
+  console.log(user);
+  res.render("Faq/faq", { faqs, user });
+});
+
+// FAQ 작성창
+router.get("/faq-add", (req, res) => {
+  const user = req.session.IsLogined;
+  res.render("Faq/faq-add", { user });
+});
+
+// FAQ 작성
+router.post("/Faq", async (req, res, next) => {
+  try {
+    let body = req.body;
+    await Faq.create({
+      question: body.inputQuestion,
+      answer: body.inputAnswer,
+      boarder: body.inputWriter,
+    });
+    res.redirect("/Faq");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// FAQ 업데이트 작성창
+router.get("/Faq/update/:postId", async (req, res, next) => {
+  const user = await req.session.IsLogined;
+  const { postId } = req.params;
+  const faq = await Faq.findOne({ where: { post_id: postId } });
+  res.render("Faq/faq-update", { faq, user });
+});
+
+// FAQ 업데이트
+router.put("/Faq/update/:postId", async (req, res, next) => {
+  try {
+    let body = req.body;
+    console.log("=========================");
+    console.log(body);
+    console.log(body.inputContent);
+    console.log("=========================");
+    await Faq.update(
+      {
+        //   post_id: this.post_id,
+        question: body.inputQuestion,
+        answer: body.inputAnswer,
+        boarder: body.inputWriter,
+        //   date: now,
+      },
+      {
+        where: { post_id: req.params.postId },
+      }
+    );
+    res.redirect("/Faq");
+    return res.status(200).send("success");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// FAQ 삭제
+router.delete("/Faq/delete/:postId", async (req, res, next) => {
+  const { postId } = req.params;
+  console.log("====================");
+  console.log(postId);
+  console.log("====================");
+  const faq = await Faq.destroy({ where: { post_id: postId } });
+  return res.send("success");
+});
 
 // 체크 해볼 부분임 ================================================================
 router.get("/Schedule", (req, res) => {
